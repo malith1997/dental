3afefcd0500248c1e56c15eef5a55e4563b74ff8
malith2cristal <malith.gayashan@cristalcode.net>
2023-02-14 00:07:58 +0530

    route the pages and create navi

---
+++ src/componets/HeroTopic.js
@@ -0,0 +1,20 @@
+import "../Styles/main.css";
+
+import { ButtonGroup,Divider,Stack,Image,Heading,Text,Button,Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+
+function Topic() {
+    return(   
+        
+        <div><section>
+            <h2 className="topic1">Implant<br></br>Dentistry</h2>
+            <Button className="BtnCard">
+            <a className="textcolor">Work With Me</a>
+          </Button>
+          </section>
+        </div>
+
+        
+    );
+}
+
+export default Topic;
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Nav from '../componets/Nav'
+import Topic from '../componets/HeroTopic'
+
+export default function Home() {
+  return (
+    <div>
+      
+
+
+      <React.Fragment>
+
+      <Nav />
+      <Topic />
+   
+    
+      
+     
+      
+    </React.Fragment>
+    </div>
+  )
+}
