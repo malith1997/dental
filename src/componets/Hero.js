79331b2930e497712d3f5eb2e158140f5c5f7852
malith2cristal <malith.gayashan@cristalcode.net>
2023-02-16 01:05:56 +0530

    hero animated

---
+++ src/componets/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import heroImg from '../Img/123.png'
+import { ButtonGroup,Divider,Stack,Image,Heading,Text,Button,Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+
+export default function Hero() {
+  return (
+    <div>
+      <Image className="img1" src={heroImg} alt="hero" />
+
+      
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from '../componets/Nav'
 import Topic from '../componets/HeroTopic'
+import Hero from '../componets/Hero'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <React.Fragment>
 
       <Nav />
+      <Hero />
       <Topic />
    
     
